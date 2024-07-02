0b31c2f99210dffdf75098008b898d856809542d
abhay-raizada <toabhayraizada@gmail.com>
2024-07-02 15:54:23 +0530

    Add Basic 30018 event fetching

--- src/components/PollResponse/FetchResults.tsx
+++ src/components/PollResponse/FetchResults.tsx
@@ -4,59 +4,96 @@ import { Event } from "nostr-tools/lib/types/core";
 import { SimplePool } from "nostr-tools";
 import { defaultRelays } from "../../nostr";
 import { useEffect, useState } from "react";
-import { Typography } from "@mui/material";
+import { MenuItem, Select, Typography } from "@mui/material";
 import { Analytics } from "../PollResults/Analytics";
 
 interface FetchResultsProps {
-  pollEvent: Event
+  pollEvent: Event;
 }
-export const FetchResults: React.FC<FetchResultsProps> = ({pollEvent}) => {
-    const [respones, setResponses] = useState<Event[] | undefined>();
-    let navigate = useNavigate();
+export const FetchResults: React.FC<FetchResultsProps> = ({ pollEvent }) => {
+  const [respones, setResponses] = useState<Event[] | undefined>();
+  const [listEvents, setListEvents] = useState<Event[] | undefined>();
+  const [selectedEvent, setSelectedEvent] = useState<Event | undefined>();
+  let navigate = useNavigate();
 
-    const getUniqueLatestEvents = (events: Event[]) => {
-      const eventMap = new Map<string, any>();
-    
-      events.forEach(event => {
-        if (!eventMap.has(event.pubkey) || event.created_at > eventMap.get(event.pubkey).created_at) {
-          eventMap.set(event.pubkey, event);
-        }
-      });
-    
-      return Array.from(eventMap.values());
+  const getUniqueLatestEvents = (events: Event[]) => {
+    const eventMap = new Map<string, any>();
+
+    events.forEach((event) => {
+      if (
+        !eventMap.has(event.pubkey) ||
+        event.created_at > eventMap.get(event.pubkey).created_at
+      ) {
+        eventMap.set(event.pubkey, event);
+      }
+    });
+
+    return Array.from(eventMap.values());
+  };
+
+  const handleResultEvent = (event: Event) => {
+    console.log("GOT EVENT", event, event.kind);
+    if (event.kind === 1070) {
+      setResponses((prevResponses) => [...(prevResponses || []), event]);
+    }
+    if (
+      event.kind === 30018 &&
+      event.tags.some((tag) => tag[0] === "e" && tag[1] === pollEvent.id)
+    ) {
+      setListEvents((prevListEvents) => [...(prevListEvents || []), event]);
+    }
+  };
+
+  const fetchPollEvents = async () => {
+    let resultFilter: Filter = {
+      "#e": [pollEvent.id],
+      kinds: [1070],
     };
+    let listFilter: Filter = {
+      "#e": [pollEvent.id],
+      kinds: [30018],
+    };
+    let pool = new SimplePool();
+    pool.subscribeMany(defaultRelays, [resultFilter, listFilter], {
+      onevent: handleResultEvent,
+    });
+    return pool;
+  };
 
-    const handleResultEvent = (event: Event) => {
-      console.log("GOT EVENT", event, event.kind)
-      if(event.kind === 1070) {
-        setResponses((prevResponses) => [...(prevResponses || []), event])
-      }
-    }
-    
-    const fetchPollEvents = async () => {
-      let resultFilter: Filter = {
-        "#e": [pollEvent.id],
-        kinds: [1070]
-      }
-      let pool = new SimplePool();
-      pool.subscribeMany(defaultRelays, [resultFilter], {
-        onevent: handleResultEvent,
+  useEffect(() => {
+    let pool: SimplePool;
+    if (!respones) {
+      fetchPollEvents().then((queryPool) => {
+        pool = queryPool;
       });
-      return pool;
     }
+    return () => {
+      if (pool) pool.close(defaultRelays);
+    };
+  }, [pollEvent]);
 
-    useEffect(() => {
-      let pool: SimplePool
-      if(!respones) { fetchPollEvents().then(
-        (queryPool) => { pool = queryPool }
-      ) }
-      return () => {
-        if(pool) pool.close(defaultRelays)
-      }
-      
-    }, [pollEvent])
+  console.log(pollEvent);
 
-    console.log(pollEvent)
-
-    return <Analytics pollEvent={pollEvent} responses={getUniqueLatestEvents(respones || [])} />
-}
+  return (
+    <>
+      <Select
+        value={selectedEvent?.id}
+        onChange={(e) =>
+          setSelectedEvent(
+            listEvents?.find((event) => event.id === e.target.value)
+          )
+        }
+      >
+        {listEvents?.map((event) => (
+          <MenuItem key={event.id} value={event.id}>
+            {event.content}
+          </MenuItem>
+        ))}
+      </Select>
+      <Analytics
+        pollEvent={pollEvent}
+        responses={getUniqueLatestEvents(respones || [])}
+      />
+    </>
+  );
+};
--- src/components/PollResults/index.tsx
+++ src/components/PollResults/index.tsx
@@ -1,68 +1,106 @@
 import { useNavigate, useParams } from "react-router-dom";
-import PollResultsTable from "./PollResultsTable"
+import PollResultsTable from "./PollResultsTable";
 import { Filter } from "nostr-tools/lib/types/filter";
 import { Event } from "nostr-tools/lib/types/core";
 import { SimplePool } from "nostr-tools";
 import { defaultRelays } from "../../nostr";
 import { useEffect, useState } from "react";
-import { Typography } from "@mui/material";
+import { MenuItem, Select, Typography } from "@mui/material";
 
 export const PollResults = () => {
-    let { eventId  } = useParams();
-    const [pollEvent, setPollEvent] = useState<Event | undefined>();
-    const [respones, setResponses] = useState<Event[] | undefined>();
-    let navigate = useNavigate();
+  let { eventId } = useParams();
+  const [pollEvent, setPollEvent] = useState<Event | undefined>();
+  const [respones, setResponses] = useState<Event[] | undefined>();
+  const [listEvents, setListEvents] = useState<Event[] | undefined>();
+  const [selectedEvent, setSelectedEvent] = useState<Event | undefined>();
+  let navigate = useNavigate();
 
-    const getUniqueLatestEvents = (events: Event[]) => {
-      const eventMap = new Map<string, any>();
-    
-      events.forEach(event => {
-        if (!eventMap.has(event.pubkey) || event.created_at > eventMap.get(event.pubkey).created_at) {
-          eventMap.set(event.pubkey, event);
-        }
-      });
-    
-      return Array.from(eventMap.values());
+  const getUniqueLatestEvents = (events: Event[]) => {
+    const eventMap = new Map<string, any>();
+
+    events.forEach((event) => {
+      if (
+        !eventMap.has(event.pubkey) ||
+        event.created_at > eventMap.get(event.pubkey).created_at
+      ) {
+        eventMap.set(event.pubkey, event);
+      }
+    });
+
+    return Array.from(eventMap.values());
+  };
+
+  const handleResultEvent = (event: Event) => {
+    console.log("GOT EVENT", event, event.kind);
+    if (event.kind === 1068) {
+      console.log("Setting poll event");
+      setPollEvent(event);
+    }
+    if (event.kind === 1070) {
+      setResponses((prevResponses) => [...(prevResponses || []), event]);
+    }
+    if (
+      event.kind === 30018 &&
+      event.tags.some((tag) => tag[0] === "e" && tag[1] === eventId)
+    ) {
+      setListEvents((prevListEvents) => [...(prevListEvents || []), event]);
+    }
+  };
+
+  const fetchPollEvents = async () => {
+    if (!eventId) {
+      alert("Invalid url");
+      navigate("/");
+    }
+    let resultFilter: Filter = {
+      "#e": [eventId!],
+      kinds: [1070],
     };
 
-    const handleResultEvent = (event: Event) => {
-      console.log("GOT EVENT", event, event.kind)
-      if(event.kind === 1068) {
-        console.log("Setting poll event")
-        setPollEvent(event)
-      }
-      if(event.kind === 1070) {
-        setResponses((prevResponses) => [...(prevResponses || []), event])
-      }
+    let pollFilter: Filter = {
+      ids: [eventId!],
+    };
+
+    let listFilter: Filter = {
+      "#e": [eventId!],
+      kinds: [30018],
+    };
+    let pool = new SimplePool();
+    pool.subscribeMany(defaultRelays, [resultFilter, pollFilter, listFilter], {
+      onevent: handleResultEvent,
+    });
+  };
+
+  useEffect(() => {
+    if (!pollEvent) {
+      fetchPollEvents();
     }
-    
-    const fetchPollEvents = async () => {
-      if(!eventId) {
-        alert("Invalid url")
-        navigate("/")
-      }
-      let resultFilter: Filter = {
-        "#e": [eventId!],
-        kinds: [1070]
-      }
+  }, [pollEvent]);
 
-      let pollFilter: Filter = {
-        ids: [eventId!]
-      }
-      let pool = new SimplePool();
-      pool.subscribeMany(defaultRelays, [resultFilter, pollFilter], {
-        onevent: handleResultEvent,
-      });
-    }
+  console.log(pollEvent);
 
-    useEffect(() => {
-      if(!pollEvent) { fetchPollEvents() }
-      
-    }, [pollEvent])
+  if (pollEvent === undefined) {
+    return <Typography>Loading...</Typography>;
+  }
 
-    console.log(pollEvent)
-
-    if(pollEvent === undefined) { return <Typography>Loading...</Typography>}
-
-    return <PollResultsTable pollEvent={pollEvent} events={getUniqueLatestEvents(respones || [])} />
-}
+  return (
+    <>
+      <Select
+        value={selectedEvent}
+        onChange={(e) =>
+          setSelectedEvent(
+            listEvents?.find((event) => event.id === e.target.value)
+          )
+        }
+      >
+        {listEvents?.map((event) => (
+          <MenuItem key={event.id}>{event.content}</MenuItem>
+        ))}
+      </Select>
+      <PollResultsTable
+        pollEvent={pollEvent}
+        events={getUniqueLatestEvents(respones || [])}
+      />
+    </>
+  );
+};
